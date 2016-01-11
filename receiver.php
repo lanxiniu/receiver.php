5655da35d32d412d0edc2c85898864f1571adcdc
ustbhuangyi <280309453@qq.com>
2016-01-11 14:11:48 +0800

    finish

---
+++ receiver.php
@@ -0,0 +1,41 @@
+<?php
+
+@error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+
+function mkdirs($path, $mod = 0777) {
+    if (is_dir($path)) {
+        return chmod($path, $mod);
+    } else {
+        $old = umask(0);
+        if(mkdir($path, $mod, true) && is_dir($path)){
+            umask($old);
+            return true;
+        } else {
+            umask($old);
+        }
+    }
+    return false;
+}
+
+if($_POST['to']){
+    $to = urldecode($_POST['to']);
+    if(is_dir($to)){
+        header("Status: 500 Internal Server Error");
+    } else {
+        if(file_exists($to)){
+            unlink($to);
+        } else {
+            $dir = dirname($to);
+            if(!file_exists($dir)){
+                mkdirs($dir);
+            }
+        }
+        if($_FILES["file"]["size"] == 0){
+            echo 0;
+        } else {
+            echo move_uploaded_file($_FILES["file"]["tmp_name"], $to) ? 0 : 1;
+        }
+    }
+} else {
+    echo 'I\'m ready for that, you know.';
+}
